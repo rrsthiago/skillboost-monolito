662aec5d0534998b9e1f37c9eb51641a1bf15eb6
rrsthiago <rrsthiago@gmail.com>
2025-01-25 00:15:18 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/com/rrsthiago/skillboost/SkillBoostApplication.java
@@ -0,0 +1,13 @@
+package com.rrsthiago.skillboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillBoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillBoostApplication.class, args);
+    }
+
+}
